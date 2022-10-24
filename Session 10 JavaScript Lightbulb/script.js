a047d91c264d04a1bca25630cef101b55435afab
WebDevMH <mhooperj@gmail.com>
2022-10-24 16:14:41 +0100

    Session 10 Bulb and BMI Challenges

---
+++ Session 10 JavaScript Lightbulb/script.js
@@ -0,0 +1,9 @@
+function turnOn() {
+  document.getElementById("myImage").src = "pic_bulbon.gif";
+}
+
+function turnOff() {
+  document.getElementById("myImage").src = "pic_bulboff.gif";
+}
+
+const bulbImage = document.getElementById("myImage");
